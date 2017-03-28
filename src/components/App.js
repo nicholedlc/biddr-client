22269db42616b56089fdaaf0ae8652ef04ae7673
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-03-27 17:27:43 -0700

    add AuctionsIndex component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import AuctionsIndex from './AuctionsIndex';
 
 const BASE_URL = 'http://localhost:3000/api'
 
@@ -24,26 +25,10 @@ class App extends Component {
 
   render() {
     const {auctions} = this.state;
-    const style={
-      margin: '1rem 1rem',
-      padding: '1rem 1rem',
-      border: 'thin solid gainsboro',
-    }
     return (
       <div className="App">
         <h1>AUCTIONS</h1>
-        <div>
-          {auctions.map(auc =>
-            <div key={auc.id} style={style}>
-              <div>
-                <strong>{auc.title}<span> #{auc.id}</span></strong>
-              </div>
-              <div>{auc.details}</div>
-              <div>{auc.endsOn}</div>
-              <div>{auc.reservePrice}</div>
-             </div>
-           )}
-        </div>
+        <AuctionsIndex auctions={auctions}/>
       </div>
     );
   }
---
+++ src/components/AuctionsIndex.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+function AuctionsIndex({auctions}) {
+  const style={
+    margin: '1rem 1rem',
+    padding: '1rem 1rem',
+    border: 'thin solid gainsboro',
+  }
+  return (
+    <div>
+      {auctions.map(auc =>
+        <div key={auc.id} style={style}>
+          <div>
+            <strong>{auc.title}<span> #{auc.id}</span></strong>
+          </div>
+          <div>{auc.details}</div>
+          <div>{auc.endsOn}</div>
+          <div>{auc.reservePrice}</div>
+         </div>
+       )}
+    </div>
+  )
+}
+
+export default AuctionsIndex;
