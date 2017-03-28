712580bacae7610de0a1a5c29fed1ef0f542d0f8
Nichole De La Cruz <nichole.dlc@gmail.com>
2017-03-27 17:06:13 -0700

    fetch and display auctions in app

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      auctions: []
+    }
+  }
+
+  componentDidMount () {
+    fetch('http://localhost:3000/api/auctions')
+      .then(r => r.json())
+      .then(({auctions}) => this.setState({auctions}))
+      .catch(console.info)
+  }
+
   render() {
+    const {auctions} = this.state;
+    const style={
+      margin: '1rem 1rem',
+      padding: '1rem 1rem',
+      border: 'thin solid gainsboro',
+    }
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>AUCTIONS</h1>
+        <div>
+          {auctions.map(auc =>
+            <div key={auc.id} style={style}>
+              <div>
+                <strong>{auc.title}<span> #{auc.id}</span></strong>
+              </div>
+              <div>{auc.details}</div>
+              <div>{auc.endsOn}</div>
+              <div>{auc.reservePrice}</div>
+             </div>
+           )}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
